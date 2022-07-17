74f1c13bd418c9f8afc24a82c670a69ec13169e8
wafa-alrizqi <wafa.alrizqi@gmail.com>
2022-07-17 09:37:14 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import "./App.css";
+import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Search from './components/Search'
 import Home from "./components/Home";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 
 function App() {
   return (
     <>
     <Router>
       <Routes>
-        <Route path='/react-project-2' exact element={<Home />} ></Route>
+        <Route path='/' exact element={<Home />} ></Route>
         <Route path='/Search' exact element={<Search />}></Route>
       </Routes>
     </Router>
